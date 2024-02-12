d5b4bb30627564b42fd89d58436a019aecc78e8a
BobikGavvv <andrey678a@gmail.com>
2024-02-12 14:56:42 +0200

    ,

--- src/main/java/com/example/springboot/models/SingleObjectModel.java
+++
@@ -1,6 +0,0 @@
-package com.example.springboot.models;
-
-public interface SingleObjectModel extends BaseModel {
-    @SuppressWarnings("unused")
-    int getId();
-}
--- src/main/java/com/example/springboot/models/api/MessageModel.java
+++
@@ -1,6 +0,0 @@
-package com.example.springboot.models.api;
-
-import com.example.springboot.models.BaseModel;
-
-public record MessageModel(Object message) implements BaseModel {
-}
